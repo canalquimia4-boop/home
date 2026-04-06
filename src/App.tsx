d3b000eaa86bc9b9ef1640a59fdbe1700cf71445
canalquimia4-boop <canalquimia4@gmail.com>
2026-04-06 18:38:00 -0300

    refactor: Rename app and remove Navbar
    Renames the application from "Waterland Pools-home" to "Home" in metadata.json.
    Removes the Navbar component from the App.tsx, as it appears to be redundant or not intended for the main application layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -248,7 +247,6 @@ export default function App() {
       <Router>
         <ScrollToTop />
         <div className="flex flex-col min-h-screen">
-          <Navbar />
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
